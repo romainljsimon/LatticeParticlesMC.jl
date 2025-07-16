63892b5179128901d7013e33eed9e0b80981e190
Romain Simon <93144534+romainljsimon@users.noreply.github.com>
2025-07-16 19:54:52 +0200

    Initial Commit

--- examples/test.jl
+++ examples/test.jl
@@ -1,17 +1,17 @@
 using LatticeParticlesMC
 using Arianna
-using StaticArrays
 
 using Distributions
 using Random
+using StaticArrays
 
 seed = 42
 rng = Xoshiro(seed)
-N = 25
+N = 100
 M = 1
 d = 3
 temperature = 1.0
-density = 1.2
+box = SVector{d, Int}(N+1, N+1, N+1)
 
 function create_bond_matrix(N::Int)
     # Create a vector to store the SVectors, each containing a pair of integers
@@ -25,32 +25,31 @@ function create_bond_matrix(N::Int)
     return matrix
 end
 
-
+position = [SVector{d, Int}(i, N÷2, N÷2) for i in 1:N]
 bonds = create_bond_matrix(N)
-position = [SVector{3, Int}(i, N÷2, N÷2) for i in 1:N]
 species = collect(1:N)
-molecule = [1 for i in 1:N]
 interaction_matrix = randn(N, N)
 interaction_matrix = 0.5 * (interaction_matrix + interaction_matrix')
-box = SVector{3, Int}(N+2, N+2, N+2)
-system = System(position, species, molecule, box, temperature, interaction_matrix, bonds)
-chains=[system]
+molecule = [1 for i in 1:N]
+
+molecules = System(position, species, molecule, box, temperature, interaction_matrix, bonds)
+chains = [molecules]
 displacement_policy = SimpleUniform()
 pool = (
     Move(Displacement(0, zero(chains[1].box), 0.0), displacement_policy, Vector{Float64}(), 0.1),
     Move(Corner(0, zero(chains[1].box), 0.0), displacement_policy, Vector{Float64}(), 0.9),
 )
 ## Define the simulation struct
-steps = 1000
+steps = 100000
 burn = 0
-block = [0, 10]
+block = [0, 1000]
 sampletimes = build_schedule(steps, burn, block)
-callbacks = (callback_energy, callback_acceptance)
+callbacks = (callback_acceptance)
 
 path = "data/test/particles/Molecules/T$temperature/N$N/M$M/seed$seed"
 algorithm_list = (
     (algorithm=Metropolis, pool=pool, seed=seed, parallel=false, sweepstep=N),
-    (algorithm=StoreCallbacks, callbacks=(callback_energy, callback_acceptance), scheduler=sampletimes),
+    (algorithm=StoreCallbacks, callbacks=(callback_acceptance, callback_energy), scheduler=sampletimes),
     (algorithm=StoreTrajectories, scheduler=sampletimes, fmt=EXYZ()),
     (algorithm=PrintTimeSteps, scheduler=build_schedule(steps, burn, steps ÷ 10))
     )
