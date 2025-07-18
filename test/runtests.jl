80068b1eebc2118bd626dc3d7d3d993726611edd
Romain Simon <93144534+romainljsimon@users.noreply.github.com>
2025-07-18 18:45:47 +0200

    Add distance test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using LatticeParticlesMC
 using Test
+using StaticArrays
 
-@testset "LatticeParticlesMC.jl" begin
+@testset "Distances" begin
+    x = SVector{3, Int}(0, 0, 1)
+    y1 = SVector{3, Int}(0, 0, 31)
+    y2 = SVector{3, Int}(105, 100, 1)
+    box = SVector{3, Int}(10, 10, 10)
+    r1 = nearest_image_distance_squared(x, y1, box)
+    r2 = nearest_image_distance_squared(x, y2, box)
+    @test r1 == 0
+    @test r2 == 25
     # Write your tests here.
 end
